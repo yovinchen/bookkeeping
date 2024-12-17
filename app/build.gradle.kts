9382e7addec7fd6973a259b0ab0819d106f54b5c
yovinchen <gzh298255@gmail.com>
2024-12-17 14:13:45 +0800

    update: 升级版本1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 6
-        versionName = "1.3.0"
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
